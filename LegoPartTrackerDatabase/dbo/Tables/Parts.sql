ba0a5645d5e0ddde68b976188d40348f42a967a8
Wayne Straub <waynestraub@gmail.com>
2018-04-28 16:01:18 -0400

    initial database schema

---
+++ LegoPartTrackerDatabase/dbo/Tables/Parts.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Parts] (
+    [Id]             INT            NOT NULL,
+    [Color]          NVARCHAR (100) NOT NULL,
+    [ElementId]      NVARCHAR (100) NULL,
+    [Name]           NVARCHAR (400) NOT NULL,
+    [PartImageUrl]   NVARCHAR (400) NOT NULL,
+    [PartNumber]     NVARCHAR (100) NOT NULL,
+    [PartUrl]        NVARCHAR (400) NULL,
+    [QuantityFound]  INT            NOT NULL,
+    [QuantityNeeded] INT            NOT NULL,
+    [SetNumber]      NVARCHAR (100) NULL,
+    CONSTRAINT [PK_Parts] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Parts_Sets_SetNumber] FOREIGN KEY ([SetNumber]) REFERENCES [dbo].[Sets] ([SetNumber])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Parts_SetNumber]
+    ON [dbo].[Parts]([SetNumber] ASC);
+
---
+++ LegoPartTrackerDatabase/dbo/Tables/Sets.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sets] (
+    [SetNumber]   NVARCHAR (100) NOT NULL,
+    [Name]        NVARCHAR (400) NOT NULL,
+    [SetImageUrl] NVARCHAR (400) NOT NULL,
+    [Theme]       NVARCHAR (400) NOT NULL,
+    [ThemeId]     INT            NOT NULL,
+    CONSTRAINT [PK_Sets] PRIMARY KEY CLUSTERED ([SetNumber] ASC)
+);
+
