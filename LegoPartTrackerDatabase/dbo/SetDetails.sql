d8dbdbdaf97680f45cfed5eda3d46a73c1583bb9
Wayne Straub <waynestraub@gmail.com>
2018-12-01 23:59:40 -0500

    added SetDetails view that adds summary part count info to set object

---
+++ LegoPartTrackerDatabase/dbo/SetDetails.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[SetDetails] AS
+select s.SetNumber, s.Name, s.SetImageUrl, s.Theme, s.ThemeId, sum(p.QuantityNeeded) 'PartsNeeded', sum(p.QuantityFound) 'PartsFound'
+from Sets s
+inner join Parts p on s.SetNumber = p.SetNumber
+group by s.SetNumber, s.Name, s.SetImageUrl, s.Theme, s.ThemeId
