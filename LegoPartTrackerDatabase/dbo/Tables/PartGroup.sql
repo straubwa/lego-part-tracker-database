a67ae6338e826fd22b03a7dee8d5454706e9d6ed
Wayne Straub <waynestraub@gmail.com>
2018-12-23 08:29:26 -0500

    added CreatedDate

--- LegoPartTrackerDatabase/dbo/Tables/PartGroup.sql
+++ LegoPartTrackerDatabase/dbo/Tables/PartGroup.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[PartGroup]
 (
     [PartNumber]	VARCHAR (100) NOT NULL,
-	[GroupId]		INT NOT NULL
+	[GroupId]		INT NOT NULL,
+    [CreatedDate] DATETIME NOT NULL, 
     CONSTRAINT [PK_PartGroup] PRIMARY KEY CLUSTERED ([PartNumber] ASC),
     CONSTRAINT [FK_PartGroup_Group_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
 )
