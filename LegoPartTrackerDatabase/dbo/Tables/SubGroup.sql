db562c0459feb8aefbe1f1043773718a530ee2a2
Wayne Straub <waynestraub@gmail.com>
2020-02-08 12:18:29 -0500

    added iconfilename to subgroup

--- LegoPartTrackerDatabase/dbo/Tables/SubGroup.sql
+++ LegoPartTrackerDatabase/dbo/Tables/SubGroup.sql
@@ -5,6 +5,7 @@
     [CreatedDate] DATETIME NOT NULL, 
     [Name] VARCHAR(400) NOT NULL,
 	[IconUrl] VARCHAR(400) NOT NULL,
+    [IconFileName] VARCHAR(400) NOT NULL, 
     CONSTRAINT [PK_Subgroup] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Subgroup_Group_GroupId] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
 )
